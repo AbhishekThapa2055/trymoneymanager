eb4dd06a890206927fb77996cf7d15cf441ce322
AbhishekThapa2055 <abhishek1999thapa@gmail.com>
2026-05-06 23:43:35 +0100

    extra line for sending mail

--- moneymanager/src/main/java/uk/abhishek/moneymanager/service/ProfileService.java
+++ moneymanager/src/main/java/uk/abhishek/moneymanager/service/ProfileService.java
@@ -47,7 +47,7 @@ public class ProfileService {
         String activationLink = activationURL+"/api/v1.0/activate?token=" + newProfile.getActivationToken();
         String  subject = "Activate your Money Manager account";
         String body = "Click on the following link to activate your account:" + activationLink;
-        emailService.sendEmail(newProfile.getEmail(), subject,body);
+
         try {
             emailService.sendEmail(newProfile.getEmail(), subject, body);
         } catch (Exception e) {
